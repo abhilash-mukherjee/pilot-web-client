19fc526aabdc2714dd680e9102239faf1437e1fc
Abhilash <abhilash301267@gmail.com>
2024-04-10 17:08:28 +0530

    modified preset values

--- src/helpers/sessionParamsPresets.js
+++ src/helpers/sessionParamsPresets.js
@@ -1,86 +1,86 @@
 export const grabAndReachoutPresets = {
     "Sideways-RIGHT": {
         boxes: [
-            { boxX: -0.424, boxZ: 0.42, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
-            { boxX: 0.424, boxZ: 0.42, label: "RED", colorLight: "#FF7755", colorDark: "#991500" }
+            { boxX: -0.424, boxZ: 0.512, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
+            { boxX: 0.424, boxZ: 0.512, label: "RED", colorLight: "#FF7755", colorDark: "#991500" }
         ],
         spheres: [
-            { spawnCentreX: -0.071, spawnCentreZ: 0.421, zoneWidth: 0.2, color: "#FF7755", label: "RED" },
-            { spawnCentreX: -0.071, spawnCentreZ: 0.421, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: 0, spawnCentreZ: 0.55, zoneWidth: 0.2, color: "#FF7755", label: "RED" },
+            { spawnCentreX: 0, spawnCentreZ: 0.55, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Sideways-LEFT": {
         boxes: [
-            { boxX: -0.424, boxZ: 0.42, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
-            { boxX: 0.424, boxZ: 0.42, label: "RED", colorLight: "#FF7755", colorDark: "#991500" }
+            { boxX: -0.424, boxZ: 0.512, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
+            { boxX: 0.424, boxZ: 0.512, label: "RED", colorLight: "#FF7755", colorDark: "#991500" }
         ],
         spheres: [
-            { spawnCentreX: -0.071, spawnCentreZ: 0.421, zoneWidth: 0.2, color: "#FF7755", label: "RED" },
-            { spawnCentreX: -0.071, spawnCentreZ: 0.421, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: 0, spawnCentreZ: 0.55, zoneWidth: 0.2, color: "#FF7755", label: "RED" },
+            { spawnCentreX: 0, spawnCentreZ: 0.55, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Diagonal-In-to-Out-LEFT": {
         boxes: [
-            { boxX: -0.336, boxZ: 0.364, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: -0.398, boxZ: 0.547, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: 0.436, spawnCentreZ: -0.066, zoneWidth: 0.15, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: 0.29, spawnCentreZ: 0.155, zoneWidth: 0.1, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Diagonal-In-to-Out-RIGHT": {
         boxes: [
-            { boxX: 0.336, boxZ: 0.364, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: 0.398, boxZ: 0.547, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: -0.436, spawnCentreZ: -0.066, zoneWidth: 0.15, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: -0.29, spawnCentreZ: 0.155, zoneWidth: 0.1, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Diagonal-Out-to-In-LEFT": {
         boxes: [
-            { boxX: 0.439, boxZ: -0.071, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
+            { boxX: 0.34, boxZ: 0.188, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: -0.198, spawnCentreZ: 0.308, zoneWidth: 0.2, color: "#FF7755", label: "BLUE" }
+            { spawnCentreX: -0.267, spawnCentreZ: 0.465, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Diagonal-Out-to-In-RIGHT": {
         boxes: [
-            { boxX: -0.439, boxZ: -0.071, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" },
+            { boxX: -0.34, boxZ: 0.188, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: 0.198, spawnCentreZ: 0.308, zoneWidth: 0.2, color: "#FF7755", label: "BLUE" }
+            { spawnCentreX: 0.267, spawnCentreZ: 0.465, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Linear-In-to-Out-RIGHT": {
         boxes: [
-            { boxX: 0.155, boxZ: 0.515, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: 0.105, boxZ: 0.58, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: 0.114, spawnCentreZ: 0.205, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: 0.086, spawnCentreZ: 0.234, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Linear-In-to-Out-LEFT": {
         boxes: [
-            { boxX: -0.068, boxZ: 0.515, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: -0.105, boxZ: 0.58, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: -0.114, spawnCentreZ: 0.205, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: -0.086, spawnCentreZ: 0.234, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Linear-Out-to-In-RIGHT": {
         boxes: [
-            { boxX: 0.158, boxZ: 0.204, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: 0.148, boxZ: 0.222, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: 0.077, spawnCentreZ: 0.48, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: 0.058, spawnCentreZ: 0.559, zoneWidth: 0.05, color: "#1E2699", label: "BLUE" }
         ]
     },
     "Linear-Out-to-In-LEFT": {
         boxes: [
-            { boxX: -0.158, boxZ: 0.204, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
+            { boxX: -0.148, boxZ: 0.222, label: "BLUE", colorLight: "#1E2699", colorDark: "#A39CFF" }
         ],
         spheres: [
-            { spawnCentreX: -0.077, spawnCentreZ: 0.48, zoneWidth: 0.2, color: "#1E2699", label: "BLUE" }
+            { spawnCentreX: -0.058, spawnCentreZ: 0.559, zoneWidth: 0.05, color: "#1E2699", label: "BLUE" }
         ]
     },
 
