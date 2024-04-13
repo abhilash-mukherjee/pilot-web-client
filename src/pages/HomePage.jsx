3016c5a4bbe2e47a1f12d5b2f5bdf508ea59f1f1
Abhilash <abhilash301267@gmail.com>
2024-04-13 19:18:06 +0530

    added logic to preload bg image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { authState } from "../state/atoms";
+import '../App.css'
 import { Box, Button, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import backgroundImage from '../assets/bgimage.png';
 
 export function HomePage() {
   const auth = useRecoilValue(authState);
@@ -17,53 +19,74 @@ export function HomePage() {
 
   return (
     <Flex 
-      height="80vh" 
+      height="auto" 
       width="100%" 
       alignItems="center" 
       justifyContent="center"
       justifySelf={'center'}
       alignSelf={'center'}
       id='test990023'
+      flexDir={'column'}
     >
-      <Box 
-        width={{ base: "90%", md: "70%", lg: "50%" }} 
-        p={4}
-      >
-        <Heading 
-          mb={4} 
-          color="teal"
-        >
-          Neuro Rehab Made Seamless & Engaging
-        </Heading>
-        <Text 
-          mb={8} 
-          color={useColorModeValue('gray.600', 'gray.200')}
-          fontSize={'1.35rem'}
-        >
-          Penguin labs builds the best in class solutions to streamline neuro rehabs, 
-          make it more scalable and engaging.
-        </Text>
-        <Flex 
-          gap={4} 
-          flexDirection={{ base: "column", sm: "row" }}
-          justifyContent={'center'}
-        >
-          <Button 
-            colorScheme="teal" 
-            variant="solid" 
-            onClick={() => navigate('/signup')}
-          >
-            Signup
-          </Button>
-          <Button 
-            colorScheme="teal" 
-            variant="outline" 
-            onClick={() => navigate('/login')}
-          >
-            Login
-          </Button>
-        </Flex>
-      </Box>
+      <Hero />
     </Flex>
   );
+}
+
+
+
+
+function Hero() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      position="relative" // Set position to relative to use absolute positioning for the overlay
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover'
+      }}
+    >
+      {/* Overlay */}
+      <Flex
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        backgroundColor="rgba(0, 0, 0, 0.45)" // Black with 45% opacity
+      />
+
+      {/* Responsive handling */}
+      <style>
+        {`
+          @media (orientation: portrait) {
+            .css-0 {
+              background-size: auto 100vh;
+            }
+          }
+
+          @media (orientation: landscape) {
+            .css-0 {
+              background-size: cover;
+            }
+          }
+        `}
+      </style>
+      <HeroContent />
+    </Flex>
+  );
+}
+
+function HeroContent(){
+  return (
+    <>
+    <Flex>
+      <Text>penguin labs</Text>
+    </Flex>
+    </>
+  )
 }
